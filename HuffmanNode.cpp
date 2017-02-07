5dbda12aae91fae3356c0556fdac9b20243a50df
dreamATD <690293245@qq.com>
2017-02-07 15:56:28 +0800

    finish the node class of Huffman tree

--- HuffmanNode.cpp
+++ HuffmanNode.cpp
@@ -3,5 +3,34 @@
 #include <algorithm>
 #include <cstring>
 
-HuffmanNode::HuffmanNode(T_val, HuffmanNode *_left, HuffmanNode *_right):
-	val(_val), left(_left), right(_right) {}
+using namespace std;
+typedef basic_string<unsigned char> ustring;
+#include "HuffmanNode.hpp"
+#include "HuffmanTree.hpp"
+
+bool operator < (const letter &a, const letter &b) {
+	return a.counter == b.counter ? a.c < b.c : a.counter < b.counter;
+}
+
+letter::letter(unsigned char _c, LL _counter): c(_c), counter(_counter) {}
+
+unsigned char letter::getChar(){
+	return c;
+}
+
+LL letter::getCounter(){
+	return counter;
+}
+void letter::initialize(unsigned char k) {
+	counter = 0;
+	c = k;
+}
+
+letter &letter::operator ++ () {
+	++counter;
+	return *this;
+}
+
+letter operator + (const letter &a, const letter &b) {
+	return letter(0, a.counter + b.counter);
+}
--- HuffmanNode.hpp
+++ HuffmanNode.hpp
@@ -1,13 +1,38 @@
 #ifndef _HuffmanNode_
 #define _HuffmanNode_
+
+#define LL long long
+
+class letter;
+
+template <class T> class HuffmanNode;
+template <class T> bool operator < (const HuffmanNode<T> &a, const HuffmanNode<T> &b);
+template <class T> class HuffmanNode;
+template <class T> class HuffmanTree;
+
+class letter{
+	friend bool operator < (const letter &a, const letter &b);
+	friend letter operator + (const letter &a, const letter &b);
+private:
+	unsigned char c;
+	LL counter;
+public:
+	letter(unsigned char _c = 0, LL _counter = 0);
+	unsigned char getChar();
+	LL getCounter();
+	void initialize(unsigned char x);
+	letter &operator ++ ();
+};
+
 template <class T>
 class HuffmanNode{
-	friend bool operator < (const HuffmanNode &a, const HuffmanNode &b);
-private:
-	T val;
-	HuffmanNode *left, *right;
+	friend bool operator < (const HuffmanNode<T> &a, const HuffmanNode<T> &b);
 public:
+	T val;
+	HuffmanNode<T> *left, *right;
+	HuffmanNode(){}
 	HuffmanNode(T _Val, HuffmanNode *_left = NULL, HuffmanNode *right = NULL);
-	
+	~HuffmanNode();
 };
+
 #endif
