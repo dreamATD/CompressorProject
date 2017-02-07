f3dee1233a37123842cb3bd8e1c3ed2607f3db54
dreamATD <690293245@qq.com>
2017-02-07 15:56:56 +0800

    finish the Huffman tree class

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -3,17 +3,80 @@
 #include <cstring>
 #include <cstdio>
 #include <queue>
-using namespace std;
+#include <fstream>
 
-struct cmp {
-	bool operator () (const *letter &a, const *letter *b) {
-		return a -> counter == b -> counter ? a -> ch < b -> ch : a -> counter < b -> counter;
+using namespace std;
+typedef basic_string<unsigned char> ustring;
+#include "HuffmanNode.hpp"
+#include "HuffmanTree.hpp"
+
+template <class T>
+struct cmp{
+	bool operator () (const T &a, const T &b){
+		return a -> val < b -> val;
 	}
 };
 
-HuffmanTree::HuffmanTree(letter *record) {
-	priority_queue<*letter, vector<*letter>, cmp> heap;
-	for(int i = 0; i < 256; ++i) {
-		heap.push(&record[i]);
+template <class T>
+void HuffmanTree<T>::buildTree(T record[]) {
+	priority_queue<HuffmanNode<T>*, vector< HuffmanNode<T>* >, cmp< HuffmanNode<T>* > > heap;
+	for (int i = 0; i < 256; ++i) {
+		if (record[i].getCounter()){
+			HuffmanNode<T> *cur = new HuffmanNode<T>(record[i], NULL, NULL);
+			heap.push(cur);
+		}
+	}
+
+	while (heap.size() > 1) {
+		HuffmanNode<T> *ch1 = heap.top(); heap.pop();
+		HuffmanNode<T> *ch2 = heap.top(); heap.pop();
+
+		HuffmanNode<T> *pnt = new HuffmanNode<T>(ch1 -> val + ch2 -> val, ch1, ch2);
+		heap.push(pnt);
+	}
+	root = heap.top();
+}
+
+template <class T>
+HuffmanTree<T>::~HuffmanTree() {
+	delete root;
+}
+
+template <class T>
+void HuffmanTree<T>::getCode(HuffmanNode<T> *cur, ustring decode[], ustring curCode) {
+	if (!cur -> left && !cur -> right) {
+		decode[cur -> val.getChar()] = curCode;
+		return;
+	}
+	if (cur -> left ) getCode(cur -> left , decode, curCode + (unsigned char)'0');
+	if (cur -> right) getCode(cur -> right, decode, curCode + (unsigned char)'1');
+}
+
+template <class T>
+HuffmanNode<T> *HuffmanTree<T>::getRoot() {
+	return root;
+}
+
+template <class T>
+void HuffmanTree<T>::translate(ifstream &in, ofstream &os, int totalCounter) {
+	unsigned char cur;
+	T *p = root;
+	while (in >> cur) {
+ 		for (int i = 7; i >= 0; --i) {
+			if (!p -> left && !p -> right) {
+				os << p -> val.getChar();
+				p = root;
+				--totalCounter;
+				if (!totalCounter) {
+					break;
+				}
+			}
+			else if (cur & (1 << i)) {
+				p = p -> right;
+			}
+			else {
+				p = p -> left;
+			}
+		}
 	}
 }
--- HuffmanTree.hpp
+++ HuffmanTree.hpp
@@ -1,13 +1,20 @@
 #ifndef _HuffmanTree_
 #define _HuffmanTree_
 
+template <class T> class HuffmanNode;
+template <class T> class HuffmanTree;
+
+template<class T>
 class HuffmanTree{
 private:
-	HuffmanNode root;
+	HuffmanNode<T> *root;
 public:
-	HuffmanTree(letter* record);
+	HuffmanTree(){}
 	~HuffmanTree();
-	translate(string code);
+	void buildTree(T *record);
+	void translate(ifstream &in, ofstream &os, int totalCounter);
+	void getCode(HuffmanNode<T> *cur, ustring decode[], ustring curCode);
+	HuffmanNode<T> *getRoot();
 };
 
 #endif
